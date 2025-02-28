26c04386e3f7d23bab056191cd670647406f606e
Brian <nicofam0802@gmail.com>
2025-02-28 21:02:26 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import customtkinter as ctk
+from ui import UI
+
+if __name__ == "__main__":
+    root = ctk.CTk()
+    root.title("Calculator")
+    root.geometry("360x720")
+    root.resizable(False,False)
+    root.config(bg="black")
+    root.iconbitmap()
+    app = UI(root)
+    root.mainloop()
---
+++ test_room.py
@@ -0,0 +1,5 @@
+class Cls:
+    x = 0
+    def __init__(self,y):
+        self.y = y
+print(Cls(2))
